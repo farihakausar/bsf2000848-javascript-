a5a6e5d50864f02dd40f0bdf95b1f43bff588d34
farihakausar <88570666+farihakausar@users.noreply.github.com>
2022-10-04 19:39:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let more=document.getElementById("moreTo")
+let moreToSE=document.getElementById("moreToSE")
+let moreToCo=document.getElementById("moreToCo")
+
+newText=()=>{
+    
+    more.innerHTML="more to see artcile writing detail Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ",
+    more.style.backgroundColor="black",
+    more.style.color="white",
+    more.style.fontFamily= 'Noto Sans Mono', monospace;
+}
+
+newTextCo=()=>{
+    
+    moreToCo.innerHTML="more to see content writing detail Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ",
+    moreToCo.style.backgroundColor="black",
+    moreToCo.style.color="white",
+    moreToCo.style.fontFamily= 'Noto Sans Mono', monospace;
+}
+newTextSE=()=>{
+    
+    moreToSE.innerHTML="more to see seo writing detail Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ",
+    moreToSE.style.backgroundColor="black",
+    moreToSE.style.color="white",
+    moreToSE.style.fontFamily= 'Noto Sans Mono', monospace;
+}
